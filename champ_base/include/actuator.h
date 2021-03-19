8dc09a1332200b92bdbe15ab12885c11ef7dc054
aditya-shirwatkar <aditya.s@students.vnit.ac.in>
2021-03-19 21:45:19 +0530

    corrected champ_base/include/actuator.h

--- champ_base/include/actuator.h
+++ champ_base/include/actuator.h
@@ -28,6 +28,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef ACTUATOR_H
 #define ACTUATOR_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 namespace champ
     {
         class Actuator
